1bb9e36f82a301f414e2cf254212afffe8d5f450
Bootify <>
2025-04-16 17:09:23 +0000

    initial commit

---
+++ src/main/java/com/artemisa/alive/AliveApplication.java
@@ -0,0 +1,14 @@
+package com.artemisa.alive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AliveApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(AliveApplication.class, args);
+    }
+
+}
